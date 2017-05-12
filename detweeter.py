0f45034e04419944c787098b73cd2ad5530d998c
RosemaryAntimony <yourdumbbaby@mail.com>
2017-05-11 22:19:39 -0400

    Add twit reading for drumpf archive

--- detweeter.py
+++ detweeter.py
@@ -1,1 +1,11 @@
 """Call detweeter modules."""
+
+
+from __future__ import print_function
+from tweet_reader import twit_reader
+
+
+twits = twit_reader()
+
+for twit in twits:
+    print (twit)
--- tweet_reader.py
+++ tweet_reader.py
@@ -1,6 +1,25 @@
 """Read the tweets."""
 
 
-def twit_reader(f_name):
+import json
+import os
+import zipfile as zf
+
+
+def twit_reader():
     """Read tweets from a file."""
-    pass
+    twits = []
+    path = "./twits/easy_d/"
+    pathos = "./twits/trump_tweet_data_archive/"
+    zippo = "master_2017.json.zip"
+
+    with zf.ZipFile(pathos + zippo, "r") as zip_rf:
+        zip_rf.extractall(path)
+
+    for fn in os.listdir(path):
+        with open(fn) as filo:
+            data = json.load(filo)
+            for obj in reversed(data):
+                twits.append(obj["text"])
+
+    return twits
